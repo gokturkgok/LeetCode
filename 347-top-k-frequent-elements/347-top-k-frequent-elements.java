e5bb44c7d8a0440fabfe3b444eab9fc758c23e04
Göktürk Gök <gokturkgok@gmail.com>
2022-09-07 03:30:27 +0300

    Time: 21 ms (43.27%), Space: 50.7 MB (44.00%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -4,15 +4,15 @@ class Solution {
         for(int num : nums)
             freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
         
-        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((e1, e2) -> (e2.getValue()-e1.getValue()));
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((e1, e2) -> (e2.getValue()-e1.getValue()));
         
         for(Map.Entry<Integer, Integer> entry: freqMap.entrySet()){
-            heap.add(entry);
+            maxHeap.add(entry);
         }
         
         int[] res = new int[k];
         for(int i = 0; i < k; i++)
-            res[i] = heap.poll().getKey();
+            res[i] = maxHeap.poll().getKey();
         
         return res;
     }
