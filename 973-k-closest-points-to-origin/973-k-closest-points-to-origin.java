d5b134ff56caaa1fda556f7786a7c98e5b582e6b
Göktürk Gök <gokturkgok@gmail.com>
2022-09-07 19:48:39 +0300

    Time: 44 ms (64.10%), Space: 115.4 MB (71.65%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -2,17 +2,6 @@ class Solution {
     
     public int[][] kClosest(int[][] points, int k) {
         
-        Comparator< int[]> distanceComparator = new Comparator<int[]>() {
-            @Override
-            public int compare(int[] p1, int[] p2) {
-                return distanceToOrigin(p1) - distanceToOrigin(p2);
-            }
-            
-            private int distanceToOrigin(int [] point) {
-                return point[0]*point[0] + point[1]*point[1];
-            }
-        };
-        
         PriorityQueue<int[]> heap = new PriorityQueue<>(points.length, (a, b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0]+b[1]*b[1]));
         
         for(int[] point : points)
