cc4a6ca66fe616c59abf3a4b3238926496a78dad
Göktürk Gök <gokturkgok@gmail.com>
2022-09-07 19:48:28 +0300

    Time: 47 ms (58.88%), Space: 115.2 MB (73.02%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -13,7 +13,7 @@ class Solution {
             }
         };
         
-        PriorityQueue<int[]> heap = new PriorityQueue<>(points.length, distanceComparator);
+        PriorityQueue<int[]> heap = new PriorityQueue<>(points.length, (a, b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0]+b[1]*b[1]));
         
         for(int[] point : points)
             heap.add(point);
