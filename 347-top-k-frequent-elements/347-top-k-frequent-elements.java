da87328e0f5ed4f64f28d39759a0202f6b8e2a5a
Göktürk Gök <gokturkgok@gmail.com>
2022-09-07 03:32:27 +0300

    Time: 21 ms (43.27%), Space: 50.3 MB (62.79%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> freqMap = new HashMap<>();
+        
         for(int num : nums)
             freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
         
